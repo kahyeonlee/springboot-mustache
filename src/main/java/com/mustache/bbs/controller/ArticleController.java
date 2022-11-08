c2a7a426692a0eeabc10a211dc0e5b2bf3770f2f
kahyun Lee <gmeoqq@gmail.com>
2022-11-09 01:22:29 +0900

    게시판 LIST까지 구현

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -2,26 +2,61 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
-
+    //spring이 ArticleRepository 구현체(ArticleDao)를 DI(인터페이스 아님)
+    //기능 - findAll(), findById(), save()
+    private final ArticleRepository articleRepository;
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
     }
 
-    @PostMapping(value = "/posts")
-    public String createArticle(ArticleDto articleDto){
-        log.info(articleDto.toString());
-        Article article = articleDto.toEntity();
-        return "";
+    @GetMapping(value = "/{id}")
+    public String selectSingle(@PathVariable long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error"; //없는 주소값을 쓰면 에러페이지
+        }
     }
+    @PostMapping(value="/posts")
+    public String add(ArticleDto articleDto) {
+        log.info(articleDto.getTitle());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId:{}", savedArticle.getId());
+        // souf %d %s
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
+    @GetMapping(value="/list")
+    public String list(Model model){
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list"; // 뷰로 반환
+    } // controller -> db -> model(객체) -> 뷰
+
+    @GetMapping("")
+    public String index(){
+        return "redirect:/articles/list";
+    }
+
 }
--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -3,23 +3,22 @@ package com.mustache.bbs.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity //jpa에서 객체로 다루겠다는 선언
 @NoArgsConstructor
 @Getter
+@Table(name="article")
 public class Article {
     @Id // entity 사용할때, 필수 primary key를 의미함
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //db에 id생성 자동으로 맡김
     private long id;
 
     private String title;
-    private String contents;
+    private String content;
 
-    public Article(String title, String contents) {
+    public Article(String title, String content) {
         this.title = title;
-        this.contents = contents;
+        this.content = content;
     }
 }
